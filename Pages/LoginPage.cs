c687ae8aa11021cd15c9b4d1f9beeff10fd09c1f
Gokul Vijayan <gokul.vijayan@experionglobal.local>
2020-05-01 11:22:13 +0530

    Added Files

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,94 @@
+﻿using Ex_haft.Configuration;
+using Ex_haft.GenericComponents;
+using Ex_haft.Utilities;
+using Ex_haft.Utilities.Reports;
+using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+
+namespace FrameworkSetup.Pages
+{
+
+    class LoginPage
+    {
+        IWebDriver driver;
+        JObject jObject;
+        public List<string> screenshotList = new List<string>();
+
+
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            jObject = ConfigFile.RetrieveUIMap("LoginPageSelector.json");
+
+        }
+
+        /// <summary>
+        /// To Verify that user is able to login to the application
+        /// </summary>
+        /// <param name="inputjson">The input json</param>
+        /// <returns>Test reports</returns>
+        public List<TestReportSteps> LoginToApplication(JToken inputjson)
+        {
+            List<TestReportSteps> listOfReport = new List<TestReportSteps>();
+            int step = 0;
+            try
+            {
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Enter username", "username", "To Verify that user is able to login to the application", step));
+                ReusableComponents.SendKeys(driver, "XPath", jObject["username"].ToString(), inputjson["username"].ToString());
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Enter password", "password", "To Verify that user is able to login to the application", step));
+                ReusableComponents.SendKeys(driver, "XPath", jObject["password"].ToString(), inputjson["password"].ToString());
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Capture screenshot", "details", "To Verify that user is able to login to the application", step));
+                screenshotList.Add(CaptureScreenshot.TakeSingleSnapShot(driver, "details")); ;
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Click loginButton", "", "To Verify that user is able to login to the application", step));
+                ReusableComponents.Click(driver, "XPath", jObject["loginButton"].ToString());
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Wait until verifyLogin is visible", "", "To Verify that user is able to login to the application", step));
+                ReusableComponents.WaitUntilElementVisible(driver, "XPath", jObject["verifyLogin"].ToString());
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Wait until loader is invisible", "", "To Verify that user is able to login to the application", step));
+                ReusableComponents.WaitUntilElementInvisible(driver, "XPath", jObject["loader"].ToString());
+                listOfReport[step].actualResultFail = "";
+                step++;
+
+                listOfReport.Add(ReusableComponents.GenerateReportSteps("Capture screenshot", "verifyLogin", "To Verify that user is able to login to the application", step));
+                screenshotList.Add(CaptureScreenshot.TakeSingleSnapShot(driver, "verifyLogin")); ;
+                listOfReport[step].actualResultFail = "";
+                step++;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("LoginToApplication failed: " + e);
+                listOfReport[step - 1].SetActualResultFail("Failed");
+            }
+            return listOfReport;
+        }
+
+
+
+        /// <summary>
+        /// Retrieve list of screenshots captured
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetLoginPageScreenshots()
+        {
+            List<string> result = screenshotList;
+            return result;
+        }
+    }
+}
---
+++ TestScript/LoginTest.cs
@@ -0,0 +1,65 @@
+﻿
+
+using Ex_haft.Configuration;
+using Ex_haft.Utilities;
+using Ex_haft.Utilities.Reports;
+using FrameworkSetup.Pages;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrameworkSetup.TestScript
+{
+    class LoginTest
+    {
+        private IWebDriver driver;
+        LoginPage loginPage;
+        JArray jsonArray;
+        string testObjective, scriptName;
+        List<string> screenshotList = new List<string>();
+        List<TestReportSteps> report;
+        public LoginTest()
+        {
+            VerifyLogin();
+        }
+
+        public void Init()
+        {
+            driver = ConfigFile.Init("Configuration\\AppSettings.json");
+            loginPage = new LoginPage(driver);
+            testObjective = "To Verify that user is able to login to the landtrack application.";
+            scriptName = "TS001_Landtrack_Login to Application";
+            jsonArray = ConfigFile.RetrieveInputTestData("LoginTest.json");
+            Constant.SetConfig("Configuration\\AppSettings.json");
+        }
+
+
+        public void VerifyLogin()
+        {
+            Init();
+            if (jsonArray != null)
+            {
+                foreach (var testData in jsonArray)
+                {
+                    report = loginPage.LoginToApplication(testData);
+                    foreach (string screenshot in loginPage.GetLoginPageScreenshots())
+                        screenshotList.Add(screenshot);
+
+
+                    Exit();
+                }
+            }
+        }
+
+        [TearDown]
+        public void Exit()
+        {
+            driver.Quit();
+            Report.WriteResultToHtml(driver, report, screenshotList, testObjective, scriptName);
+        }
+
+    }
+}
